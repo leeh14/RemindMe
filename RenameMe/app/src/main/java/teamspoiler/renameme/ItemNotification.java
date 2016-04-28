fb6d2a8c62473e908049b043190c9ef0889ea85a
hirats <hirats@rpi.edu>
2016-04-28 00:14:45 -0400

    Added comments to notification class

--- RenameMe/app/src/main/java/teamspoiler/renameme/ItemNotification.java
+++ RenameMe/app/src/main/java/teamspoiler/renameme/ItemNotification.java
@@ -5,11 +5,8 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.Binder;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
@@ -27,14 +24,9 @@ import teamspoiler.renameme.DataElements.*;
  */
 public class ItemNotification extends Service {
     DatabaseHelperClass db;
-    /*protected BroadcastReceiver stopServiceReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            stopSelf();
-        }
-    };*/
 
-    // Use this method to create a new notification
+    // Use this method to create an Alarm that goes off at the given time and
+    //sets off the ItemNotification Service. Call it whenever an item is added or modified
     public static void notify(Context context, Item item) {
         Intent myIntent = new Intent(context, ItemNotification.class);
         myIntent.putExtra("Item_ID", item.getID());
@@ -47,11 +39,14 @@ public class ItemNotification extends Service {
         alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
     }
 
+    //Use this method to remove a notification from the Android device if one exists.
+    //Call it when an item is viewed.
     public static void denotify(Context context, Item item) {
         NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         mNotificationManager.cancel(item.getID());
     }
 
+    //
     protected Notification buildNotification(Item item) {
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
@@ -59,15 +54,12 @@ public class ItemNotification extends Service {
                         .setContentTitle("RemindMe")
                         .setContentText(item.getName() + " at " + item.getDate().toString());
 
-        //registerReceiver(stopServiceReceiver, new IntentFilter("notification_stopper"));
-        //PendingIntent stopIntent = PendingIntent.getBroadcast(this, 0, new Intent("notification_stopper"), PendingIntent.FLAG_UPDATE_CURRENT);
 
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
         // Adds the back stack for the Intent (but not the Intent itself)
         stackBuilder.addParentStack(CategoriesActivity.class);
         // Adds the Intent that starts the Activity to the top of the stack
-        //Intent.get
-        //stackBuilder.addNextIntent(new Intent(stopServiceReceiver));
+
         // Creates an explicit intent for an Activity in your app
         Intent categoryIntent = new Intent(this, CategoryActivity.class);
         categoryIntent.putExtra("Category_ID", item.getCategoryID());
@@ -80,9 +72,6 @@ public class ItemNotification extends Service {
 
         mBuilder.setContentIntent(resultPendingIntent);
 
-        // mId allows you to update the notification later on.
-
-        //mBuilder.setContentIntent(stopIntent);
         return mBuilder.build();
     }
 
@@ -91,6 +80,8 @@ public class ItemNotification extends Service {
         db = DatabaseHelperClass.getInstance(this);
     }
 
+    //Method that is called when the AlarmTimer runs out and the Service starts
+    //Creates a corresponding notification if it the current time correpsonds to item's expiration date.
     @Override
     public int onStartCommand(Intent intent, int flags, int startID) {
         int mId = intent.getExtras().getInt("Item_ID");
@@ -100,6 +91,7 @@ public class ItemNotification extends Service {
         Duration window = new Duration(5*60*1000);
         if (item != null && now.isAfter(item.getDate().minus(window)) && now.isBefore(item.getDate().plus(window)))  {
             NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            // mId allows you to update the notification later on by creating or cancelling a notification with the same ID.
             mNotificationManager.notify(mId, buildNotification(item));
             stopSelf();
             return 1;
